ae63892458c85bc7b0a23ddd2f770f7743b6f876
Artem Lytvynenko <om31.artem.lytvynenko@gmail.com>
2026-05-04 14:51:09 +0300

    feat: add TariffUnlimited class
    add constructor that calls the parent constructor and initializes new variable

---
+++ src/main/java/TariffUnlimited.java
@@ -0,0 +1,18 @@
+import java.time.LocalDate;
+
+
+public class TariffUnlimited extends Tariff{
+    private LocalDate expirationDate;
+
+    public TariffUnlimited(String name, double subsCost, int clientCount, LocalDate expirationDate) {
+        super(name, subsCost, clientCount);
+        LocalDate today = LocalDate.now();
+        if (expirationDate.isEqual(today)){
+            throw new IllegalArgumentException("The expiration date cannot be the same date it is created");
+        }
+        if(expirationDate.isBefore(today)){
+            throw new IllegalArgumentException("The expiration date cannot be in the past");
+        }
+        this.expirationDate = expirationDate;
+    }
+}
