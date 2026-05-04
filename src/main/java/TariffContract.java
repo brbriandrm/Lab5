29c1c2967e6333ff8f4453bb83d78e59ddab2d6a
Artem Lytvynenko <om31.artem.lytvynenko@gmail.com>
2026-05-04 14:40:16 +0300

    feat: add TariffContract class
    add constructor that calls the parent constructor and initializes new variable

---
+++ src/main/java/TariffContract.java
@@ -0,0 +1,11 @@
+public class TariffContract extends Tariff{
+    private int durationMonths;
+
+    public TariffContract(String name, double subsCost, int clientCount, int durationMonths) {
+        super(name, subsCost, clientCount);
+        if(durationMonths <= 1) {
+            throw new IllegalArgumentException("Duration of contract cannot be less than 1 month");
+        }
+        this.durationMonths = durationMonths;
+    }
+}
